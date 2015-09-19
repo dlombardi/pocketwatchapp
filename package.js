196f0d2872b4be42cf598933179b92f4a986b38c
Darien Lombardi <darien.lombardi@gmail.com>
2015-09-19 16:20:00 -0700

    initial commit

---
+++ package.js
@@ -0,0 +1,16 @@
+{
+  "name": "learn-ng",
+  "version": "1.0.0",
+  "description": "",
+  "main": "webpack.config.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "babel-core": "^5.8.24",
+    "babel-loader": "^5.3.2",
+    "webpack": "^1.12.1"
+  }
+}

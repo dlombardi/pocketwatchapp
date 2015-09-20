9a2bb189ba2b7fc672c8542eb22e80c47614e291
Benjamin Huang <coolconfucius@yahoo.com>
2015-09-19 18:39:57 -0700

    loginService.js

---
+++ login.js
@@ -0,0 +1,41 @@
+app.service("authService", function(email, password) {
+
+  console.log('userCtrl loaded');
+
+  var ref = new Firebase("https://tc-pocketwatch.firebaseio.com");
+
+
+  this.createUser = ref.createUser({
+    email    : email,
+    password : password
+  }, function(error, userData) {
+    if (error) {
+      alert("Error creating user:", error);
+    } else {
+      alert("Successfully created user account with uid:", userData.uid);
+    }
+  });
+
+
+  this.userLogin = ref.authWithPassword({
+    email    : email,
+    password : password
+  }, function(error, authData) {
+    if (error) {
+      alert("Login Failed!", error);
+    } else {
+      alert("Authenticated successfully with payload:", authData);
+    }
+  },{
+    remember: "sessionOnly"
+  });
+
+});
+
+
+
+
+
+
+
+

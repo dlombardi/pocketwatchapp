0c39bacce75a4750a0769dee84f9dab354a5053d
Alice Wu <alicewu1992@gmail.com>
2015-09-19 23:04:59 -0700

    Scan Zip function
    scans through zip codes to find disaster

--- src/services/SMSService.js
+++ src/services/SMSService.js
@@ -17,7 +17,28 @@ sendMessage()
 // })
 
 
-
+var warnings = [];
+function scana(){
+  var arr = [94539, 82301]
+  arr.forEach(function(item) {
+    getWarnings(item)}
+    );
+  function getWarnings(zip) {
+    // var warnings = [];
+    var promise = $.getJSON("http://api.wunderground.com/api/7a10e87424a9a29a/alerts/q/" + zip + ".json");
+    promise.success(function(alerts) {
+      console.log(alerts)
+      if(typeof alerts.alerts[0].description !== 'undefined'){
+        warnings.push({zip: alerts.alerts[0].description});
+        console.log(alerts.alerts[0].tz_long)
+        // sendMessage(/*zip, alerts.alerts[0].description, phone*/);
+      }
+    });
+    promise.fail(function(error) {
+      console.log('error:', error);
+    });
+  }
+}
 
 // var accountSid = 'AC74dd59800c3f1b9b013a88b08d486a72';
 // var authToken = '790383efee4c34e89288e8ea55ab7855';

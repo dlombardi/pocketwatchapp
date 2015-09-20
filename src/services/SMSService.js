c1557819204f1df5b983d00dc9ee2da917b4e1f7
Austin Ezell <austin.r.ezell@gmail.com>
2015-09-20 03:27:50 -0700

    re-establish the SMSService as standalone

--- src/services/SMSService.js
+++ src/services/SMSService.js
@@ -1,16 +1,15 @@
-app.service('SMSService', function(city_name, alert){
+app.service('SMSService', function(phoneNumber, alert){
     const accountSid = 'AC74dd59800c3f1b9b013a88b08d486a72';
     const authToken = '790383efee4c34e89288e8ea55ab7855';
-    const twilPhnNum = '+18182736634'
+    const twilPhnNum = '+18182736634';
 
     var client = require('twilio')(accountSid, authToken);
 
-    var message = "whatever"
-    sendMessage = function(phoneNumber){
+    this.sendMessage = function(){
         client.messages.create({
             to: phoneNumber,
             from: twilPhnNum,
-            body: `${message}`,
+            body: alert,
         }, function(err, message) {
             console.log(message.sid);
         });

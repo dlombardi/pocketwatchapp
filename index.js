b9a9653670a63715d50bbadbfd57560555a9cd2d
Darien Lombardi <darien.lombardi@gmail.com>
2015-09-20 21:28:29 -0700

    adding backend data structure, pulling from firebase

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var morgan = require('morgan');
+var Firebase = require("firebase");
 
-const TWILIO_ACCOUNT_SID = process.env.FIREBASE_REF;
-const TWILIO_AUTHTOKEN = process.env.TWILIO_ACCOUNT_SID;
-const TWILIO_PHONE_NUMBER = process.env.TWILIO_AUTHTOKEN;
-const FIREBASE_REF = process.env.TWILIO_PHONE_NUMBER;
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTHTOKEN = process.env.TWILIO_AUTHTOKEN;
+const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
+const FIREBASE_REF = process.env.FIREBASE_REF;
 
 var app = express();
 app.use(morgan());
@@ -16,6 +17,25 @@ app.get('*', function(req, res) {
 app.listen(process.env.PORT || 3000);
 
 function sendAlerts(){
+
+  var ref = new Firebase(FIREBASE_REF);
+
+  var userData = {};
+
+  ref.on("value", function(snapshot) {
+    var users = snapshot.val().users;
+    Object.keys(users).forEach(function(key) {
+      var user = users[key];
+      userData[user.phone] = [];
+      if(user.zips){
+        Object.keys(user.zips).forEach(function(key){
+          userData[user.phone].push(user.zips[key]);
+        });
+      }
+    });
+  }, function (errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
 
 };
 

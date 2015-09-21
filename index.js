852fae30f90044fc3960672d67f996ce3b580de9
Darien Lombardi <darien.lombardi@gmail.com>
2015-09-20 22:57:55 -0700

    commented certain items in index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// var express = require('express');
-// var morgan = require('morgan');
-// var Firebase = require("firebase");
-//
-// const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
-// const TWILIO_AUTHTOKEN = process.env.TWILIO_AUTHTOKEN;
-// const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
-// const FIREBASE_REF = process.env.FIREBASE_REF;
-//
-// var app = express();
-// app.use(morgan());
-//
-// app.get('*', function(req, res) {
-//   res.sendfile(__dirname + '/' + req.params[0]);
-// });
-//
-// app.listen(process.env.PORT || 3000);
-//
+var express = require('express');
+var morgan = require('morgan');
+var Firebase = require("firebase");
+
+const TWILIO_ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+const TWILIO_AUTHTOKEN = process.env.TWILIO_AUTHTOKEN;
+const TWILIO_PHONE_NUMBER = process.env.TWILIO_PHONE_NUMBER;
+const FIREBASE_REF = process.env.FIREBASE_REF;
+
+var app = express();
+app.use(morgan());
+
+app.get('*', function(req, res) {
+  res.sendfile(__dirname + '/' + req.params[0]);
+});
+
+app.listen(process.env.PORT || 3000);
+
 // function sendAlerts(){
 //
 //   var ref = new Firebase(FIREBASE_REF);
